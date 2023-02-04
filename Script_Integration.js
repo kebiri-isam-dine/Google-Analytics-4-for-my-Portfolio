09811842c4fdbaef5d62488a418435754c590456
KEBIRI Isam <kebiri.isam.dine@gmail.com>
2023-02-04 19:08:37 +0100

    first commit

---
+++ Script_Integration.js
@@ -0,0 +1,11 @@
+// Voici la balise Google pour ce compte. Copiez-la et collez-la dans le code de chaque page de votre site, directement après l'élément <head>. N'ajoutez qu'une seule balise Google par page.
+
+< !--Google tag(gtag.js)-- >
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-CKRFLCDH53"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-CKRFLCDH53');
+</script>
